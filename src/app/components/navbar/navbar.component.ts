3b7f139002adc09d5041f9a7ab341f5f490590a2
ElvisRD <elvisAppsdev@gmail.com>
2025-11-03 03:43:32 -0300

    added navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  device: string = '';
+  isOpen: boolean = false;
+
+  ngOnInit(): void {
+    this.checkDevice();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkDevice();
+  } 
+
+  checkDevice(): void {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      if(width <= 480){
+        this.device = 'phone';
+      }else if(width > 480 && width <= 768){
+        this.device = 'tablet';
+      }else{
+        this.device = 'desktop';
+      }
+  }
+
+  openMenu(): void {
+    this.isOpen = !this.isOpen;
+  }
+
 
 }
