1d37a9839f11f8dd8fcb20abe23e90c845e1c72d
ElvisRD <elvisAppsdev@gmail.com>
2025-11-04 19:56:08 -0300

    added card-proyects component

---
+++ src/app/components/card-proyects/card-proyects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-proyects',
+  imports: [],
+  templateUrl: './card-proyects.component.html',
+  styleUrl: './card-proyects.component.scss'
+})
+export class CardProyectsComponent {
+
+}
