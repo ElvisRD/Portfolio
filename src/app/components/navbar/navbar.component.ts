90c49dad2eed7a5ee4af1cb6e00740fef59afd7b
ElvisRD <elvisAppsdev@gmail.com>
2025-11-04 17:30:29 -0300

    solved problems section home

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,7 +8,6 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  device: string = '';
   isOpen: boolean = false;
 
   ngOnInit(): void {
@@ -22,18 +21,8 @@ export class NavbarComponent {
 
   checkDevice(): void {
       const width = window.innerWidth;
-      const height = window.innerHeight;
 
-      if(width <= 640){
-        this.device = 'phone';
-        this.isOpen = false;
-      }else if(width <= 768){
-        this.device = 'tablet';
-        this.isOpen = true;
-      }else{
-        this.device = 'desktop';
-        this.isOpen = true;
-      }
+      this.isOpen = width <= 640 ? false : true; 
   }
 
   openMenu(): void {
